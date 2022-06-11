05fcd96c4455bd596f968a815175992fafa92a7c
Kamil Kolasa <truzgavka@gmail.com>
2022-06-11 12:19:11 +0200

    Created view

---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module JPGtoBITMAPconverter {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
 public class Controller {
+
+    @FXML
+    private BorderPane mainBorderPane;
+
+    public void initialize() {
+
+    }
+
+    @FXML
+    public void showSelectWindow() {
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(mainBorderPane.getScene().getWindow());
+    }
 }
