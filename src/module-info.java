33823087317bdae732d0d3a001aa29d824539cc2
Kamil Kolasa <truzgavka@gmail.com>
2022-06-23 21:20:08 +0200

    Added image view with scaling.

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,8 @@
 module JPGtoBITMAPconverter {
     requires javafx.fxml;
     requires javafx.controls;
+    requires java.desktop;
+    requires javafx.swing;
 
     opens sample;
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,15 +1,22 @@
 package sample;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
 
+import javax.imageio.ImageIO;
 import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
 
 public class Controller {
 
     @FXML
     private BorderPane mainBorderPane;
+    @FXML
+    private ImageView imageToConvert;
 
     public void initialize() {
 
@@ -18,6 +25,18 @@ public class Controller {
     @FXML
     public void showSelectWindow() {
         FileChooser fileChooser = new FileChooser();
+//        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("JPG, PNG","*.jpg, *.png"));
         File selectedFile = fileChooser.showOpenDialog(mainBorderPane.getScene().getWindow());
+
+        if(selectedFile != null) {
+            BufferedImage img = null;
+            try {
+                img = ImageIO.read(selectedFile);
+            } catch (IOException e) {
+            }
+            imageToConvert.setImage(SwingFXUtils.toFXImage(img, null));
+        }
+
+
     }
 }
