da92153ffa2adf7805946d61a83887073fad5872
Kamil Kolasa <truzgavka@gmail.com>
2022-08-03 14:41:21 +0200

    Fixed error with modifying start image. Resolved problem with start image need. Added confimation window when exit.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,9 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Slider;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -13,6 +16,7 @@ import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 import java.awt.image.BufferedImage;
+import java.util.Optional;
 
 public class Controller {
 
@@ -27,14 +31,17 @@ public class Controller {
     private int sliderValue;
 
     public void initialize() {
+
         slider.valueProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                sliderValue = t1.intValue();
-                imageConverter.convertImage(sliderValue);
+                if(imageView.getImage() != null) {
+                    sliderValue = t1.intValue();
+                    imageConverter.convertImage(sliderValue);
 
-                System.out.println(sliderValue);
-                imageView.setImage(imageConverter.getCurrentImage());
+                    System.out.println(sliderValue);
+                    imageView.setImage(imageConverter.getCurrentImage());
+                }
             }
         });
 
@@ -55,7 +62,19 @@ public class Controller {
             //inizjalizacja i użycie ImageConvertera
             imageConverter = new ImageConverter(img);
             imageView.setImage(imageConverter.getCurrentImage());
+            slider.setValue(0.0);
         }
     }
 
+    @FXML
+    public void exitApplication() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Warning");
+        alert.setHeaderText("Are you sure you want to exit application?");
+        alert.setContentText("Not saved progress will be losted.");
+        Optional<ButtonType> result = alert.showAndWait();
+        if(result.isPresent() && result.get() == ButtonType.OK) {
+            System.exit(0);
+        }
+    }
 }
